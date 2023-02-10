d77420008668ccc026cd0405e08f254d202696f7
Ilmir Bakirov <bakirov@dev.vtb.ru>
2023-02-10 18:07:24 +0300

    extract to separate file

--- wiki.go
+++ wiki.go
@@ -1,35 +1,14 @@
 package main
 
 import (
-	"errors"
 	"html/template"
 	"log"
 	"net/http"
-	"os"
 	"regexp"
 )
 
 var templates = template.Must(template.ParseFiles("templates/edit.html", "templates/view.html"))
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
-
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-func (p *Page) save() error {
-	filename := "pages/" + p.Title + ".txt"
-	return os.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := "pages/" + title + ".txt"
-	body, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
 
 func main() {
 	http.HandleFunc("/view/", makeHandler(viewHandler))
